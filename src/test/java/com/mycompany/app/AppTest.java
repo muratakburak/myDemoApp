aa6a70d1ca93991789cdccbf4d15052a25ea7ccc
Mehmet Murat Akburak <muratakburakma@gmail.com>
2018-10-09 10:51:49 +0300

    Fixing typos. AppTest.java updated.

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -53,31 +53,31 @@ public class AppTest
     public void testSimple() {
         ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList( 1 , 2 , 3 , 4 ));
         ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList(5, 6, 7, 8));
-        String[] answers = {"1","2","5","6","7"} ;
+        String[] answers = {"[1, ","2, ","5, ","6, ","7]"} ;
         assertTrue(check( answers , new App().computeMyResult(arr1, arr2, 2 , 3)));
     }
     public void testAnotherSimple() {
         ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList( 131 , 221 , 34 , 41 ));
         ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList( 415, 61, 73, 85));
-        String[] answers = {"131","221","415","61","73"} ;
+        String[] answers = {"[131, ","221, ","415, ","61, ","73]"} ;
         assertTrue(check( answers , new App().computeMyResult(arr1, arr2, 2 , 3)));
     }
     public void testLengthsAreLower() {
         ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList( 7 , 1 ));
         ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList(12, 4, 5));
-        String[] answers = {"7","1","12","4","5" } ;
+        String[] answers = {"[7, ","1, ","12, ","4, ","5]" } ;
         assertTrue(check( answers , new App().computeMyResult(arr1, arr2, 12 , 132)));
     }
     public void testAnotherLengthsAreLower() {
         ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList( 4542 , 3453 , 3246, 2342 ));
         ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList( 2341, 474, 9567 , 467));
-        String[] answers = {"4542","3453","3246","2342","2341" , "474" , "9567" , "467" } ;
+        String[] answers = {"[4542, ","3453, ","3246, ","2342, ","2341, " , "474, " , "9567, " , "467]" } ;
         assertTrue(check( answers , new App().computeMyResult(arr1, arr2, 12423 , 1322)));
     }
     public void testZero() {
         ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList( 41 , 123 ));
         ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList( 1234 , 414 , 2325 , 23 , 2));
-        String[] answers = {"1234","414","2325","23","2" } ;
+        String[] answers = {"[1234, ","414, ","2325, ","23, ","2]" } ;
         assertTrue(check( answers , new App().computeMyResult(arr1, arr2, 0 , 7)));
     }
 
